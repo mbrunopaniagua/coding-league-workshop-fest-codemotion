945151823f89bd147ed39195e8ebdf1c1ce47ce3
Borja Asensio Calvo <borja.asensio.calvo@ing.com>
2023-11-21 14:53:12 +0100

    challenge2_D_top

--- challenge2/src/main/java/ChecksumCalculator.java
+++ challenge2/src/main/java/ChecksumCalculator.java
@@ -3,18 +3,10 @@ import java.util.Arrays;
 public class ChecksumCalculator {
 
     public static int calculateChecksum(int[][] matrix) {
-        int result = 0;
-
-        for (int[] ints : matrix) {
-            int minValue = Arrays.stream(ints).min().getAsInt();
-            int maxValue = Arrays.stream(ints).max().getAsInt();
-
-            int different = maxValue - minValue;
-
-            result += different;
-        }
-
-        return result;
+        return Arrays.stream(matrix)
+            .mapToInt(ints ->
+                Arrays.stream(ints).max().orElse(0) - Arrays.stream(ints).min().orElse(0)
+            ).sum();
     }
 
 }
