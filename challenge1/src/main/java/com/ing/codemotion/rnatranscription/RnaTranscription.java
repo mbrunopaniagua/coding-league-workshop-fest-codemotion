4f5de38c679809b5007b94f07da1d8a201fac357
Manuel Regino Márquez Bonilla <manuel.regino.marquez.bonilla@ing.com>
2023-11-21 16:56:19 +0100

    Implemented solution to pass all tests

--- challenge1/src/main/java/com/ing/codemotion/rnatranscription/RnaTranscription.java
+++ challenge1/src/main/java/com/ing/codemotion/rnatranscription/RnaTranscription.java
@@ -1,8 +1,17 @@
 package com.ing.codemotion.rnatranscription;
 
+import java.util.stream.Collectors;
+
 public class RnaTranscription {
 
     String transcribe(String dnaStrand) {
+        return dnaStrand.chars()
+                .mapToObj(c -> String.valueOf((char) c))
+                .map(this::getRnaComplement)
+                .collect(Collectors.joining());
+    }
+
+    private String getRnaComplement(String dnaStrand) {
         return switch (dnaStrand) {
             case "G" -> "C";
             case "C" -> "G";
