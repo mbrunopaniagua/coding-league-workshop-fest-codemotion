ad009ce45270eb3eec1097707ce248592a57bcc0
Manuel Regino Márquez Bonilla <manuel.regino.marquez.bonilla@ing.com>
2023-11-21 16:51:45 +0100

    Implemented solution to pass first two tests

--- challenge1/src/main/java/com/ing/codemotion/rnatranscription/RnaTranscription.java
+++ challenge1/src/main/java/com/ing/codemotion/rnatranscription/RnaTranscription.java
@@ -3,6 +3,9 @@ package com.ing.codemotion.rnatranscription;
 public class RnaTranscription {
 
     String transcribe(String dnaStrand) {
+        if (dnaStrand.equals("G")){
+            return "C";
+        }
         return "";
     }
 }
