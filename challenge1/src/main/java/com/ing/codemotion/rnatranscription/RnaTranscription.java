6f35f691a8d07699b87878cf8b21803a00dc5723
Manuel Regino Márquez Bonilla <manuel.regino.marquez.bonilla@ing.com>
2023-11-21 16:55:12 +0100

    Implemented solution to pass all tests with a single nucleotide

--- challenge1/src/main/java/com/ing/codemotion/rnatranscription/RnaTranscription.java
+++ challenge1/src/main/java/com/ing/codemotion/rnatranscription/RnaTranscription.java
@@ -3,11 +3,12 @@ package com.ing.codemotion.rnatranscription;
 public class RnaTranscription {
 
     String transcribe(String dnaStrand) {
-        if (dnaStrand.equals("G")){
-            return "C";
-        } else if (dnaStrand.equals("C")){
-            return "G";
-        }
-        return "";
+        return switch (dnaStrand) {
+            case "G" -> "C";
+            case "C" -> "G";
+            case "T" -> "A";
+            case "A" -> "U";
+            default -> "";
+        };
     }
 }
